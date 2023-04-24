bd0af33589ed8cc2dd8dc1e4c069542cb7451bbf
Вислобоков Денис Владимирович <dvvislobokov@sberbank.ru>
2023-04-24 13:27:50 +0300

    add new types: bool, float32, float64, int

--- env.go
+++ env.go
@@ -1,6 +1,10 @@
 package env
 
-import "os"
+import (
+	"os"
+	"strconv"
+	"strings"
+)
 
 func String(name, defaultValue string) string {
 	value := os.Getenv(name)
@@ -8,4 +12,32 @@ func String(name, defaultValue string) string {
 		return defaultValue
 	}
 	return value
+}
+
+func Int(name string, defaultValue int) int {
+	value, err := strconv.Atoi(os.Getenv(name))
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
+
+func Float32(name string, defaultValue float32) float32 {
+	value, err := strconv.ParseFloat(os.Getenv(name), 32)
+	if err != nil {
+		return defaultValue
+	}
+	return float32(value)
+}
+
+func Float(name string, defaultValue float64) float64 {
+	value, err := strconv.ParseFloat(os.Getenv(name), 64)
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
+
+func Bool(name string) bool {
+	return strings.ToLower(os.Getenv(name)) == "true"
 }
